d1844a3ccc006da4406516d4dbe3f65f30258c08
Matei <mateimurarasu@yahoo.com>
2024-04-01 09:56:34 +0300

    Add automation script

---
+++ rundir.sh
@@ -0,0 +1,13 @@
+!#usr/bin
+# Preluam data curenta in formaul DD-MM
+current_date=$(date +'%d.%m')
+# Cream fisierele
+mkdir $current_date
+cd $current_date
+touch index.html
+touch style.css
+mkdir res
+mkdir res/icons
+mkdir res/images
+echo "Executat cu succes!"
+echo "Creat de Mat(2.718)i"
